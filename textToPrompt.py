465ce57638d17b623a6c83d3d8e69330ab550e5d
Friedrich Commichau <f.commichau@hm.edu>
2024-05-12 18:48:54 +0200

    add functions for better reusability

--- textToPrompt.py
+++ textToPrompt.py
@@ -1,35 +1,35 @@
 import requests
 import json
 
-# URL for the Ollama API endpoint
-url = 'http://localhost:11434/api/generate'
+def send_post_request(url, data):
+    response = requests.post(url, json=data, stream=True)
+    return response
 
-# Data to send in the POST request, change the 'model' if you have a different model name
+def handle_streaming_response(response):
+    prompt = 'Generate an image displaying the following resources: '
+    if response.status_code == 200:
+        try:
+            for chunk in response.iter_lines():
+                if chunk:
+                    decoded_chunk = json.loads(chunk.decode('utf-8'))
+                    prompt += decoded_chunk['response']
+                    if decoded_chunk.get('done', False):
+                        return prompt
+        except Exception as e:
+            print("Error while streaming the data:", e)
+            return None
+    else:
+        print("Failed to get response, status code:", response.status_code)
+        return None
+
+# Usage example:
+url = 'http://localhost:11434/api/generate'
 data = {
-    "model": "promptEng",  # This should match the model name your Ollama server is configured to use
-    "prompt": "Pencil",
+    "model": "promptEng",
+    "prompt": "the only object here is the iphone x",
     "max_tokens": 50
 }
-
-# Send a POST request to the server with stream enabled
-response = requests.post(url, json=data, stream=True)
-
-prompt = 'Generate an image displaying the following resources: '
-
-# Check if the request was successful
-if response.status_code == 200:
-    print("Streaming data from model:")
-    try:
-        for chunk in response.iter_lines():
-            if chunk:  
-                decoded_chunk = json.loads(chunk.decode('utf-8'))
-                prompt += decoded_chunk['response']
-
-                if decoded_chunk.get('done', False):
-                    print(prompt)
-                    break
-
-    except Exception as e:
-        print("Error while streaming the data:", e)
-else:
-    print("Failed to get response, status code:", response.status_code)
+response = send_post_request(url, data)
+final_prompt = handle_streaming_response(response)
+if final_prompt:
+    print(final_prompt)
