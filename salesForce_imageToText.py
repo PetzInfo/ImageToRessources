c7fe39ced8f917f7b46610ad4777e8e491f879a9
Friedrich Commichau <f.commichau@hm.edu>
2024-05-12 18:31:59 +0200

    isolated image description from warnings

--- salesForce_imageToText.py
+++ salesForce_imageToText.py
@@ -1,6 +1,11 @@
 from PIL import Image
 from transformers import BlipProcessor, BlipForConditionalGeneration
+import warnings
 
+# Suppress warnings if you don't want to see them in your console
+warnings.filterwarnings('ignore', category=UserWarning, module='transformers')
+
+# Set up the image captioning model
 processor = BlipProcessor.from_pretrained("Salesforce/blip-image-captioning-large")
 model = BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-captioning-large")
 
@@ -8,8 +13,12 @@ img_path = './images/take3.jpeg'  # Make sure the path is correct and accessible
 raw_image = Image.open(img_path).convert('RGB')  # Open the image directly
 
 # Conditional image captioning
-text = "the main objekt here is "
+text = "The only object here is "
 inputs = processor(raw_image, text, return_tensors="pt")
 
-out = model.generate(**inputs)
-print(processor.decode(out[0], skip_special_tokens=True))
+# Control the generation length by specifying max_new_tokens
+out = model.generate(**inputs, max_new_tokens=20)
+image_description = processor.decode(out[0], skip_special_tokens=True)
+
+# Only print the final description
+print(image_description)
