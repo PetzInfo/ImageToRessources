3e6e772167e1cfaafa2df330712cf9d3431f7436
Friedrich Commichau <f.commichau@hm.edu>
2024-05-15 19:21:30 +0200

    add timer to loop

--- main.py
+++ main.py
@@ -1,21 +1,25 @@
 from salesForce_imageToText import load_model, generate_image_description
 from textToPrompt import send_post_request, handle_streaming_response
 from captureImage import capture_image
+import time
 
 # Load the model once
 processor, model = load_model()
 
 # Main loop
 while True:
+    start = time.time()
     if not capture_image():  # Check if user chose to exit during capture
         print("Exiting...")
         break
 
     # Generate description
     img_path = './images/captured_image.jpg'
-    text = "The only object here is "
+    text = "The only one object here is a: "
     description = generate_image_description(img_path, text, processor, model)
+    timeTillDescription = time.time()
     print(description)
+    print(f"Time taken to generate description: {timeTillDescription - start:.2f} seconds")
 
     # Send description to prompt generation service
     url = 'http://localhost:11434/api/generate'
@@ -29,6 +33,8 @@ while True:
     if final_prompt:
         print(final_prompt)
 
+    final = time.time()
+    print(f"Whole time taken: {final - start:.2f} seconds")
     # Ask the user if they want to continue
     if input("Capture another image? (y/n): ").strip().lower() != 'y':
         print("Exiting...")
